10d405a48aa76702c22afbaa22afadd193158285
Khai Nguyen <1192669+khaister@users.noreply.github.com>
2025-09-03 17:07:59 -0700

    Prettier

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,29 +1,31 @@
 import React from 'react';
 
 function GuessInput() {
-  const [input, setInput] = React.useState("");
+  const [input, setInput] = React.useState('');
   const handleSubmit = (e) => {
-    console.log("You guessed:", input);
-    setInput("");
-  }
-  return <>
-  <form className="guess-input-wrapper" action={handleSubmit}>
-    <label htmlFor="guess-input">Enter guess:</label>
-    <input
-      id="guess-input"
-      type="text"
-      name="guess"
-      pattern="[a-zA-Z]{5}"
-      title="5 letter word"
-      required
-      minLength="5"
-      maxLength="5"
-      value={input}
-      onChange={(e) => setInput(e.target.value.toUpperCase())}
-      style={{ textTransform: "uppercase" }}
-    />
-  </form>
-  </>;
+    console.log('You guessed:', input);
+    setInput('');
+  };
+  return (
+    <>
+      <form className="guess-input-wrapper" action={handleSubmit}>
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+          id="guess-input"
+          type="text"
+          name="guess"
+          pattern="[a-zA-Z]{5}"
+          title="5 letter word"
+          required
+          minLength="5"
+          maxLength="5"
+          value={input}
+          onChange={(e) => setInput(e.target.value.toUpperCase())}
+          style={{ textTransform: 'uppercase' }}
+        />
+      </form>
+    </>
+  );
 }
 
 export default GuessInput;
